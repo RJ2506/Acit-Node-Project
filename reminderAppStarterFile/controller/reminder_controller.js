b6c5c8cdb2038e87964f11cf36a5e806c28fdb46
clinton <kwong526@my.bcit.ca>
2021-10-28 12:00:25 -0700

    fixed the empty boolean value for completed

--- reminderAppStarterFile/controller/reminder_controller.js
+++ reminderAppStarterFile/controller/reminder_controller.js
@@ -1,65 +1,65 @@
 let database = require("../database");
 
 let remindersController = {
-    list: (req, res) => {
-        res.render("reminder/index", { reminders: database.cindy.reminders });
-    },
+  list: (req, res) => {
+    res.render("reminder/index", { reminders: database.cindy.reminders });
+  },
 
-    new: (req, res) => {
-        res.render("reminder/create");
-    },
+  new: (req, res) => {
+    res.render("reminder/create");
+  },
 
-    listOne: (req, res) => {
-        let reminderToFind = req.params.id;
-        let searchResult = database.cindy.reminders.find(function(reminder) {
-            return reminder.id == reminderToFind;
-        });
-        if (searchResult != undefined) {
-            res.render("reminder/single-reminder", { reminderItem: searchResult });
-        } else {
-            res.render("reminder/index", { reminders: database.cindy.reminders });
-        }
-    },
+  listOne: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    if (searchResult != undefined) {
+      res.render("reminder/single-reminder", { reminderItem: searchResult });
+    } else {
+      res.render("reminder/index", { reminders: database.cindy.reminders });
+    }
+  },
 
-    create: (req, res) => {
-        let reminder = {
-            id: database.cindy.reminders.length + 1,
-            title: req.body.title,
-            description: req.body.description,
-            completed: false,
-        };
-        database.cindy.reminders.push(reminder);
-        res.redirect("/reminders");
-    },
+  create: (req, res) => {
+    let reminder = {
+      id: database.cindy.reminders.length + 1,
+      title: req.body.title,
+      description: req.body.description,
+      completed: false,
+    };
+    database.cindy.reminders.push(reminder);
+    res.redirect("/reminders");
+  },
 
-    edit: (req, res) => {
-        let reminderToFind = req.params.id;
-        let searchResult = database.cindy.reminders.find(function(reminder) {
-            return reminder.id == reminderToFind;
-        });
-        res.render("reminder/edit", { reminderItem: searchResult });
-    },
+  edit: (req, res) => {
+    let reminderToFind = req.params.id;
+    let searchResult = database.cindy.reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    res.render("reminder/edit", { reminderItem: searchResult });
+  },
 
-    update: (req, res) => {
-        let reminderToUpdate = req.params.id;
-        database.cindy.reminders[reminderToUpdate - 1] = {
-            id: reminderToUpdate,
-            title: req.body.title,
-            description: req.body.description,
-            completed: req.body.completed,
-        };
-        res.redirect("/reminders");
-    },
+  update: (req, res) => {
+    let reminderToUpdate = req.params.id;
+    database.cindy.reminders[reminderToUpdate - 1] = {
+      id: reminderToUpdate,
+      title: req.body.title,
+      description: req.body.description,
+      completed: req.body.completed === "true",
+    };
+    res.redirect("/reminders");
+  },
 
-    delete: (req, res) => {
-        // Implement this code
-        let reminderToDelete = req.params.id;
-        let reminderIndex = database.cindy.reminders.findIndex(
-            (item) => item.id == reminderToDelete
-        );
-        database.cindy.reminders.splice(reminderIndex, 1);
-        res.redirect("/reminders");
-    },
+  delete: (req, res) => {
+    // Implement this code
+    let reminderToDelete = req.params.id;
+    let reminderIndex = database.cindy.reminders.findIndex(
+      (item) => item.id == reminderToDelete
+    );
+    database.cindy.reminders.splice(reminderIndex, 1);
+    res.redirect("/reminders");
+  },
 };
 
 module.exports = remindersController;
