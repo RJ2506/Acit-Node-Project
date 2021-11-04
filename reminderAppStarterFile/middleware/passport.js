d544be2f83a5803ed00c720c10d161be8471ae6b
Rodolf John Gayem <rjgayem@gmail.com>
2021-11-04 11:21:21 -0700

    added githubstrategy

--- reminderAppStarterFile/middleware/passport.js
+++ reminderAppStarterFile/middleware/passport.js
@@ -1,33 +1,48 @@
 const passport = require("passport");
 const LocalStrategy = require("passport-local").Strategy;
 const userController = require("../controller/userController");
-const localLogin = new LocalStrategy(
-  {
-    usernameField: "email",
-    passwordField: "password",
-  },
-  (email, password, done) => {
-    const user = userController.getUserByEmailIdAndPassword(email, password);
-    return user
-      ? done(null, user)
-      : done(null, false, {
-          message: "Your login details are not valid. Please try again",
-        });
-  }
+const GitHubStrategy = require("passport-github2").Strategy;
+
+const githubLogin = passport.use(
+    new GitHubStrategy({
+            clientID: process.env.clientID,
+            clientSecret: process.env.clientSecret,
+            callbackURL: "http://localhost:3001/auth/github/callback",
+        },
+        (accessToken, refreshToken, profile, done) => {
+            return done(null, profile);
+        }
+    )
 );
 
-passport.serializeUser(function (user, done) {
-  // The function where a session is created
-  done(null, user.id);
+const localLogin = passport.use(
+    new LocalStrategy({
+            usernameField: "email",
+            passwordField: "password",
+        },
+        (email, password, done) => {
+            const user = userController.getUserByEmailIdAndPassword(email, password);
+            return user ?
+                done(null, user) :
+                done(null, false, {
+                    message: "Your login details are not valid. Please try again",
+                });
+        }
+    )
+);
+
+passport.serializeUser(function(user, done) {
+    // The function where a session is created
+    done(null, user.id);
 });
 
-passport.deserializeUser(function (id, done) {
-  let user = userController.getUserById(id);
-  if (user) {
-    done(null, user);
-  } else {
-    done({ message: "User not found" }, null);
-  }
+passport.deserializeUser(function(id, done) {
+    let user = userController.getUserById(id);
+    if (user) {
+        done(null, user);
+    } else {
+        done({ message: "User not found" }, null);
+    }
 });
 
-module.exports = passport.use(localLogin);
+module.exports = { localLogin, githubLogin };
