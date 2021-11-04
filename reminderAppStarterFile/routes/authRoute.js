148cdfa097f2c1f34acbf47a33d8e5cb98297da3
Rodolf John Gayem <rjgayem@gmail.com>
2021-11-04 10:20:03 -0700

    added a route for the logout

--- reminderAppStarterFile/routes/authRoute.js
+++ reminderAppStarterFile/routes/authRoute.js
@@ -1,34 +1,37 @@
 const express = require("express");
 const passport = require("../middleware/passport");
 const { forwardAuthenticated } = require("../middleware/checkAuth");
-let { database } = require("../models/userModel.js")
+let { database } = require("../models/userModel.js");
 
 const router = express.Router();
 
-router.get("/login", forwardAuthenticated, (req, res) => res.render("auth/login"));
+router.get("/login", forwardAuthenticated, (req, res) =>
+    res.render("auth/login")
+);
 router.get("/register", (req, res) => res.render("auth/register"));
 
+router.get("/logout", (req, res) => {
+    req.logout();
+    res.redirect("/auth/login");
+});
+
 router.post(
-  "/login",
-  passport.authenticate("local", {
-    successRedirect: "/reminders",
-    failureRedirect: "/auth/login",
-  })
+    "/login",
+    passport.authenticate("local", {
+        successRedirect: "/dashboard",
+        failureRedirect: "/auth/login",
+    })
 );
 
 router.post("/register", (req, res) => {
-
-  database.push({
-      id: database.length + 1,
-      name: req.body.name,
-      email: req.body.email,
-      password: req.body.password
-
-  })
-  database["password"] = req.body.password;
-  res.render("auth/login");
-}
-)
-
+    database.push({
+        id: database.length + 1,
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+    });
+    database["password"] = req.body.password;
+    res.render("auth/login");
+});
 
 module.exports = router;
