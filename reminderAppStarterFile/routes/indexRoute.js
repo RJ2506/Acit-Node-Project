c2c4d17e30d88d59ede5fffb16605b23268d65c3
Rodolf John Gayem <rjgayem@gmail.com>
2021-11-09 21:25:06 -0800

    remove the root page and isAdmin

--- reminderAppStarterFile/routes/indexRoute.js
+++ reminderAppStarterFile/routes/indexRoute.js
@@ -1,10 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
-
-router.get("/", (req, res) => {
-    res.send("welcome");
-});
+const { ensureAuthenticated } = require("../middleware/checkAuth");
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
     res.render("dashboard", {
