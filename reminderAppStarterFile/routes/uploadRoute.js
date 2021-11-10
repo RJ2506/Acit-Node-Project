8e76abfd3826e5d8e57fb4e97efa4f650dc9a8f7
Rodolf John Gayem <rjgayem@gmail.com>
2021-11-09 21:25:49 -0800

    added the ensureAuthenticated for the upload route

--- reminderAppStarterFile/routes/uploadRoute.js
+++ reminderAppStarterFile/routes/uploadRoute.js
@@ -1,25 +1,26 @@
 const express = require("express");
+const { ensureAuthenticated } = require("../middleware/checkAuth");
+
 const router = express.Router();
+
 const multer = require("multer");
 const pictures = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, "public");
     },
     filename: (req, file, cb) => {
-        console.log(file);
         cb(null, file.originalname);
     },
 });
 
 const upload = multer({ storage: pictures });
-router.get("/upload", (req, res) => {
+router.get("/upload", ensureAuthenticated, (req, res) => {
     res.render("upload");
 });
 
 router.post("/upload", upload.single("image"), (req, res) => {
     const profile = req.user;
     profile.photo = "/" + req.file.filename;
-    console.log(profile);
     res.render("dashboard", { user: profile });
 });
 
