bd44be2cc48633f102a65cd05affb23f860e70ba
Rodolf John Gayem <rjgayem@gmail.com>
2021-11-10 16:49:49 -0800

    remove the root page

--- reminderAppStarterFile/routes/indexRoute.js
+++ reminderAppStarterFile/routes/indexRoute.js
@@ -1,25 +1,17 @@
 const express = require("express");
 const router = express.Router();
-<<<<<<< HEAD
-const { ensureAuthenticated } = require("../middleware/checkAuth");
-=======
 const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
 
-router.get("/", (req, res) => {
-  res.send("welcome");
-});
->>>>>>> main
-
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
-  res.render("dashboard", {
-    user: req.user,
-  });
+    res.render("dashboard", {
+        user: req.user,
+    });
 });
 
 router.get("/admin", isAdmin, (req, res) => {
-  res.render("admin", {
-    user: req.user,
-  });
+    res.render("admin", {
+        user: req.user,
+    });
 });
 
 module.exports = router;
