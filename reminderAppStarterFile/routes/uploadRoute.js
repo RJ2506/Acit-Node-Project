db31aeaa3139fcb03db2fba41f41f83f4fd821e9
Rodolf John Gayem <rjgayem@gmail.com>
2021-11-10 16:50:14 -0800

    created a route for upload

--- reminderAppStarterFile/routes/uploadRoute.js
+++ reminderAppStarterFile/routes/uploadRoute.js
@@ -1,27 +1,50 @@
 const express = require("express");
-const { ensureAuthenticated } = require("../middleware/checkAuth");
-
-const router = express.Router();
-
+const path = require("path");
+const helmet = require("helmet");
+const morgan = require("morgan");
+const cors = require("cors");
 const multer = require("multer");
-const pictures = multer.diskStorage({
-    destination: (req, file, cb) => {
-        cb(null, "public");
-    },
+const imgur = require("imgur");
+const fs = require("fs");
+
+const { ensureAuthenticated } = require("../middleware/checkAuth");
+const { json } = require("body-parser");
+
+const storage = multer.diskStorage({
+    destination: "public/uploads",
     filename: (req, file, cb) => {
-        cb(null, file.originalname);
+        cb(
+            null,
+            file.fieldname + "-" + Date.now() + path.extname(file.originalname)
+        );
     },
 });
+const upload = multer({ storage: storage });
+const router = express.Router();
+router.use(cors());
+router.use(morgan("dev"));
+router.use(helmet());
 
-const upload = multer({ storage: pictures });
+router.use(express.urlencoded({ extended: true }));
+router.use(json({ extended: false }));
+
+router.use(upload.any());
+
 router.get("/upload", ensureAuthenticated, (req, res) => {
     res.render("upload");
 });
 
-router.post("/upload", upload.single("image"), (req, res) => {
+router.post("/upload", async(req, res) => {
     const profile = req.user;
-    profile.photo = "/" + req.file.filename;
-    res.render("dashboard", { user: profile });
+    const file = req.files[0];
+    try {
+        const url = await imgur.uploadFile(`public/uploads/${file.filename}`);
+        profile.photo = url.link;
+        fs.unlinkSync(`public/uploads/${file.filename}`);
+        res.redirect("/dashboard");
+    } catch (err) {
+        console.log("error", err);
+    }
 });
 
 module.exports = router;
