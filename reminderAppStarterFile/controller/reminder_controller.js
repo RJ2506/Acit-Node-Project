abbdd1ea6c8053dd7a2453a77e53c8b479cef11d
kwong526 <kwong526@my.bcit.ca>
2021-11-16 15:07:08 -0800

    added random reminder_id
    added random reminder id for each reminder instead of using .length

--- reminderAppStarterFile/controller/reminder_controller.js
+++ reminderAppStarterFile/controller/reminder_controller.js
@@ -2,88 +2,88 @@ let database = require("../database");
 const user = require("../models/userModel").database;
 
 let remindersController = {
-    list: (req, res) => {
-        const username = req.user.name.replace(" ", "_");
-        res.render("reminder/index", {
-            reminders: database[username].reminders,
-        });
-    },
+  list: (req, res) => {
+    const username = req.user.name.replace(" ", "_");
+    res.render("reminder/index", {
+      reminders: database[username].reminders,
+    });
+  },
 
-    new: (req, res) => {
-        res.render("reminder/create");
-    },
+  new: (req, res) => {
+    res.render("reminder/create");
+  },
 
-    revoke: (req, res) => {
-        let sessionToDestroy = req.params.id;
-        // console.log(sessionToDestroy);
-        // console.log(Object.keys(req.session.store));
-        req.sessionStore.destroy(sessionToDestroy, function(err) {
-            console.log(err);
-        });
-        res.render("adminDash", { session: req.sessionStore.sessions });
-    },
+  revoke: (req, res) => {
+    let sessionToDestroy = req.params.id;
+    // console.log(sessionToDestroy);
+    // console.log(Object.keys(req.session.store));
+    req.sessionStore.destroy(sessionToDestroy, function (err) {
+      console.log(err);
+    });
+    res.render("adminDash", { session: req.sessionStore.sessions });
+  },
 
-    admin: (req, res) => {
-        // console.log(JSON.parse(req.sessionStore.sessions));
-        res.render("adminDash", { session: req.sessionStore.sessions });
-    },
-    //to view the reminder
-    listOne: (req, res) => {
-        let reminderToFind = req.params.id;
-        const username = req.user.name.replace(" ", "_");
-        let searchResult = database[username].reminders.find(function(reminder) {
-            return reminder.id == reminderToFind;
-        });
-        if (searchResult != undefined) {
-            res.render("reminder/single-reminder", { reminderItem: searchResult });
-        } else {
-            res.render("reminder/index", { reminders: database[username].reminders });
-        }
-    },
+  admin: (req, res) => {
+    // console.log(JSON.parse(req.sessionStore.sessions));
+    res.render("adminDash", { session: req.sessionStore.sessions });
+  },
+  //to view the reminder
+  listOne: (req, res) => {
+    let reminderToFind = req.params.id;
+    const username = req.user.name.replace(" ", "_");
+    let searchResult = database[username].reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    if (searchResult != undefined) {
+      res.render("reminder/single-reminder", { reminderItem: searchResult });
+    } else {
+      res.render("reminder/index", { reminders: database[username].reminders });
+    }
+  },
 
-    create: (req, res) => {
-        const username = req.user.name.replace(" ", "_");
-        let reminder = {
-            id: database[username].reminders.length + 1,
-            title: req.body.title,
-            description: req.body.description,
-            completed: false,
-        };
-        database[username].reminders.push(reminder);
-        res.redirect("/reminders");
-    },
+  create: (req, res) => {
+    const username = req.user.name.replace(" ", "_");
+    let reminder = {
+      id: Math.random(10000),
+      title: req.body.title,
+      description: req.body.description,
+      completed: false,
+    };
+    database[username].reminders.push(reminder);
+    res.redirect("/reminders");
+  },
 
-    edit: (req, res) => {
-        let reminderToFind = req.params.id;
-        const username = req.user.name.replace(" ", "_");
-        let searchResult = database[username].reminders.find(function(reminder) {
-            return reminder.id == reminderToFind;
-        });
-        res.render("reminder/edit", { reminderItem: searchResult });
-    },
+  edit: (req, res) => {
+    let reminderToFind = req.params.id;
+    const username = req.user.name.replace(" ", "_");
+    let searchResult = database[username].reminders.find(function (reminder) {
+      return reminder.id == reminderToFind;
+    });
+    res.render("reminder/edit", { reminderItem: searchResult });
+  },
 
-    update: (req, res) => {
-        const username = req.user.name.replace(" ", "_");
-        let reminderToUpdate = req.params.id;
-        database[username].reminders[reminderToUpdate - 1] = {
-            id: reminderToUpdate,
-            title: req.body.title,
-            description: req.body.description,
-            completed: req.body.completed === "true",
-        };
-        res.redirect("/reminders");
-    },
+  update: (req, res) => {
+    const username = req.user.name.replace(" ", "_");
+    let reminderToUpdate = req.params.id;
+    database[username].reminders[reminderToUpdate - 1] = {
+      id: reminderToUpdate,
+      title: req.body.title,
+      description: req.body.description,
+      completed: req.body.completed === "true",
+    };
+    res.redirect("/reminders");
+  },
 
-    delete: (req, res) => {
-        // Implement this code
-        const username = req.user.name.replace(" ", "_");
-        let reminderToDelete = req.params.id;
-        let reminderIndex = database[username].reminders.findIndex(
-            (item) => item.id == reminderToDelete
-        );
-        database[username].reminders.splice(reminderIndex, 1);
-        res.redirect("/reminders");
-    },
+  delete: (req, res) => {
+    // Implement this code
+    const username = req.user.name.replace(" ", "_");
+    let reminderToDelete = req.params.id;
+    let reminderIndex = database[username].reminders.findIndex(
+      (item) => item.id == reminderToDelete
+    );
+    database[username].reminders.splice(reminderIndex, 1);
+    res.redirect("/reminders");
+  },
 };
 
 module.exports = remindersController;
