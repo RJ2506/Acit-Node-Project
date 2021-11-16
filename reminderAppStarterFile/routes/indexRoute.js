5bd71670e3e5e62441c259f0b30bd17c9435df56
Rodolf John Gayem <rjgayem@gmail.com>
2021-11-15 21:02:38 -0800

    added an if statement if the user is already in the reminders database

--- reminderAppStarterFile/routes/indexRoute.js
+++ reminderAppStarterFile/routes/indexRoute.js
@@ -1,8 +1,20 @@
 const express = require("express");
 const router = express.Router();
 const { ensureAuthenticated, isAdmin } = require("../middleware/checkAuth");
+let reminder_database = require("../database");
 
 router.get("/dashboard", ensureAuthenticated, (req, res) => {
+    const username = req.user.name.replace(" ", "_");
+    let searchResult = reminder_database[username];
+    console.log(searchResult);
+    if (searchResult) {
+        res.render("dashboard", {
+            user: req.user,
+        });
+    }
+    reminder_database[username] = {
+        reminders: [],
+    };
     res.render("dashboard", {
         user: req.user,
     });
